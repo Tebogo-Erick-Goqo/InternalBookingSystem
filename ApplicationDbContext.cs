a8b66cacaee283f21fa316f0c96cdc0e088c2912
Tebogo Goqo <tebogoerickgoqo@gmail.com>
2025-07-15 00:30:41 +0200

    Resource CRUD and Bookings CRUD

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Microsoft.EntityFrameworkCore;
+using InternalBookingSystem.Models;
+
+namespace InternalBookingSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Booking>()
+                .HasOne(b => b.Resource)
+                .WithMany(r => r.Bookings)
+                .HasForeignKey(b => b.ResourceId);
+        }
+    }
+}
+
---
+++ InternalBookingSystem/Models/Booking.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InternalBookingSystem.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        [Required]
+        public int ResourceId { get; set; }
+        
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public DateTime EndTime { get; set; }
+        [Required]
+        public string BookedBy { get; set; }
+        [Required]
+        public string Purpose { get; set; }
+        [Required]
+        public Resource Resource { get; set; }
+        public bool IsCancelled { get; set; } = false;
+    }
+}
